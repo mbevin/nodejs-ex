d5c0a3dad141bf14d9c4f27c66b179c3fa4f705c
Michael Bevin <mbevin@gmail.com>
2019-10-18 22:02:06 +0300

    Added compression.

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
+var compression = require('compression');
+app.use(compression());
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
@@ -103,7 +106,7 @@ app.get('/', function (req, res) {
     });
 
   } else {
-    res.render('index.html', { dbLatest: null });
+    res.render('index.html', { dbLatest: null, dbNumItems: null });
   }
 });
 
